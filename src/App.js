1d5bbbc00f010e4c0ed9b2e6584d93c07248aa61
Rico Fajar Afandi <ricof.afandi@gmail.com>
2025-01-22 16:28:55 +0700

    initial project and button element

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "assets/scss/style.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React App</h1>
+    </div>
+  );
+}
+
+export default App;
